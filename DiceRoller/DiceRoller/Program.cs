96bd073f0e80812ed89d374f8fc950e62ce3601c
Jessica Gates <gatespj@outlook.com>
2024-01-31 22:13:11 -0500

    all pieces working

--- DiceRoller/DiceRoller/Program.cs
+++ DiceRoller/DiceRoller/Program.cs
@@ -1,35 +1,46 @@
 ﻿
 using System.Collections.Specialized;
 using System.ComponentModel;
+using System.Runtime.Intrinsics.Arm;
 using System.Security.Cryptography;
 
 int validDiceSides = 0;
+string playAgain = "";
 
-do
-{
-    Console.Write("Please enter the number of sides for a pair of dice: ");
-    string diceSides = Console.ReadLine();
-    validDiceSides = CheckValidInteger(diceSides);
-    if (validDiceSides == -1)
+do {
+    do
     {
-        Console.WriteLine("That is not a valid number, try again!");
+        Console.Write("Please enter the number of sides for a pair of dice: ");
+        string diceSides = Console.ReadLine();
+        validDiceSides = CheckValidInteger(diceSides);
+        if (validDiceSides == -1)
+        {
+            Console.WriteLine("That is not a valid number, try again!");
+        }
+    } while (validDiceSides == -1);
+
+    Console.WriteLine("Roll the dice!");
+
+    Random randomGenerator = new Random();
+
+    int randomDice1 = randomGenerator.Next(1, validDiceSides);
+    int randomDice2 = randomGenerator.Next(1, validDiceSides);
+    int sum = CalculateSum(randomDice1, randomDice2);
+    Console.WriteLine($"You rolled a {randomDice1} and a {randomDice2} for a total of {sum}!");
+    if (validDiceSides == 6)
+    {
+        Console.WriteLine(LuckyDice(randomDice1, randomDice2));
+        Console.WriteLine(FinalResult(randomDice1, randomDice2));
     }
-} while (validDiceSides == -1);
 
-Console.WriteLine("Roll the dice!");
+    Console.WriteLine("Would you like to play again? (yes/no)");
+    playAgain = Console.ReadLine().ToLower().Trim();
 
-Random randomGenerator = new Random();
-
-int randomDice1 = randomGenerator.Next(1, validDiceSides);
-int randomDice2 = randomGenerator.Next(1, validDiceSides);
-int sum = CalculateSum(randomDice1, randomDice2);
-Console.WriteLine($"You rolled a {randomDice1} and a {randomDice2} for a total of {sum}!");
+} while (playAgain == "yes"); //need to add validation on this answer
+Console.WriteLine("Thanks for playing!");
 
 
-
-
-
-static int CheckValidInteger(string diceSides)
+int CheckValidInteger(string diceSides)
 {
     int returnValue = 0;
     try
@@ -43,26 +54,36 @@ static int CheckValidInteger(string diceSides)
     return returnValue;
 }
 
-static int CalculateSum(int firstNumber, int secondNumber)
+int CalculateSum(int firstNumber, int secondNumber)
 {
     int sum = firstNumber + secondNumber;
     return sum;
 }
 
+string LuckyDice(int firstNumber, int secondNumber)
+{
+    if (firstNumber == 1 && secondNumber == 1)
+        return $"You got snake eyes!";
+    if (firstNumber == 1 && secondNumber == 2 || firstNumber == 2 && secondNumber == 1)
+        return $"You rolled Ace Deuce!";
+    if (firstNumber == 6 && secondNumber == 6)
+        return $"You rolled Box Cars!";
+    else return "";
+}
 
+string FinalResult(int firstNumber, int secondNumber)
+{
+    if (firstNumber + secondNumber == 7 || firstNumber + secondNumber == 11)
+        return $"Win!";
+    if (firstNumber + secondNumber == 2 || firstNumber + secondNumber == 3 || firstNumber + secondNumber == 12)
+        return $"Craps!";
+    else return "" ;
+}
 
 Console.ReadKey();
 
 
 
-// output
-// snake eyes: two 1s
-// ace deuce: a 1 and 2 
-// box cars: two 6s
-// win: a total of 7 or 11 
-// craps: a total of 2,3, or 12 (will also generate another message)
 // extra: come up w/ winning combos for other dice sizes beside 5 
-
-// prompt user to roll dice again y/n 
 
 
